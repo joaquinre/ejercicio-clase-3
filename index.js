4c4fabd32cffc496692be1d0a446b2ed1134a6d0
Joaquin Re <joaquinre123@gmail.com>
2023-05-23 09:37:54 -0300

    add outstandingPlayers & removeSocers functions

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const filteredPlayersResult = (scores) => {
   return scores.filter(score => score > 7)
 }
 
+const outstandingPlayers = (scores, players) => {
+  const outstandingScores = filteredPlayersResult(scores)
+  return players.filter((_, index) => outstandingScores.includes(scores[index]))
+}
+
+const removeOutstandingScores = (scores) => {
+  return scores.filter(score => score <= 7)
+}
 // 
 // 
 // function evalPerformance(scores, players) {
