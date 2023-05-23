620b64795e74242b07543d5fbd74f03bc035eb29
Joaquin Re <joaquinre123@gmail.com>
2023-05-23 09:23:26 -0300

    implementacion de funcion filterPlayers

---
+++ index.js
@@ -0,0 +1,16 @@
+const scores = [6, 9.3, 4.5, 8, 9]
+const players = ['Alvarez', 'Martinez', 'Molina', 'De Paul']
+
+// filtrar puntaje mayor a 7
+const filteredPlayersResult = (scores) => {
+  return scores.filter(score => score > 7)
+}
+
+// 
+// 
+// function evalPerformance(scores, players) {
+  // const outstandingPlayers = 
+  // const regularPlayers =
+// }
+
+
